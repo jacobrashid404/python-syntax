e6a17f73df0e8c9f512eac4b97960317bf0a704b
Jacob <jacobrashid99@gmail.com>
2024-04-22 12:07:19 -0700

    any7 implemented

--- any7.py
+++ any7.py
@@ -1,7 +1,17 @@
 def any7(nums):
     """Are any of these numbers a 7? (True/False)"""
 
-    # YOUR CODE HERE 
+    # YOUR CODE HERE
+    # for num in nums:
+    #     if num == 7:
+    #         return True
+    # return False
+
+    # if nums.count(7) > 0:
+    #     return True
+    # return False
+
+    return True if nums.count(7) > 0 else False
 
 
 print("should be true", any7([1, 2, 7, 4, 5]))
