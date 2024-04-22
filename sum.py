69336952f83d98f7c9e2fd6e67f094036cd3a0d0
Anya Aven <leafeon96@gmail.com>
2024-04-22 14:55:26 -0400

    finished

--- sum.py
+++ sum.py
@@ -6,12 +6,21 @@ def sum_nums(nums):
 
     Should return (not print):
       10
-    """  
+    """
 
     # Python has a built-in function `sum()` for this, but we don't
     # want you to use it. Please write this by hand.
 
     # YOUR CODE HERE
+
+    if True:
+        final_count = 0 # function scoped so this still works
+
+    for num in nums:
+        final_count += num
+
+    return final_count
+
 
 
 print("sum_nums returned", sum_nums([1, 2, 3, 4]))
